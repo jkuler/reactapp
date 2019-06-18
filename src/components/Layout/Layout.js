d6a796053bf7ce337d50185151b92d0b944770b1
prozadmin <koolmx9@gmail.com>
2019-06-18 14:56:23 -0500

    adding toggle function

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,11 +9,18 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
     
     sideDrawerCloseHandler = () => {
        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+           return { showSideDrawer: !prevState.showSideDrawer}
+        });
+        
     }
   
 
@@ -21,7 +28,7 @@ class Layout extends Component {
 
         return (
             <Aux>
-            <Toolbar />
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
             <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />  
             <main className={classes.Content}>
                 {this.props.children}
