d1a25863fa60e5a6229a4eedae98a1e6e59418a0
josue kula <koolmx9@gmail.com>
2019-07-01 14:06:27 -0500

    Adding a spinner and error message

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react';
 import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css'
 import * as actions from '../../store/actions/index';
 
@@ -101,7 +102,7 @@ switchAuthModeHandler = () => {
             })
         }
 
-        const form = formElementsArray.map(formElement => {
+        let form = formElementsArray.map(formElement => {
             return (<Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -115,8 +116,18 @@ switchAuthModeHandler = () => {
             );
                
         })
+        if (this.props.loading) {
+            form = <Spinner />
+        }
+        let errorMessage = null;
+        if (this.props.error){
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}> 
                 { form }
                 <Button btnType="Success">Submit</Button>
@@ -128,6 +139,14 @@ switchAuthModeHandler = () => {
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    };
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
@@ -135,4 +154,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
