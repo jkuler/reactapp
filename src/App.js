9661af4b402c5f01777236f9a22f23b9bf0f1816
josue kula <koolmx9@gmail.com>
2019-07-01 20:42:04 -0500

    fixing the redirect

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ class App extends Component {
                <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/logout" component={Logout} />
+            <Route path="/auth" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
             <Redirect to="/" />
           </Switch>
